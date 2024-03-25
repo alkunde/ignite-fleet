ead53cf2dee394dd85bc5929e5070a558b309210
André Kunde <andrelbkunde@gmail.com>
2024-03-25 16:27:29 -0300

    mongodb connection

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { AppProvider, UserProvider } from "@realm/react";
+
+import { REALM_APP_ID } from "@env";
 
 import theme from "./src/theme";
 import { SignIn } from "./src/screens/SignIn";
+import { Home } from "./src/screens/Home";
 import { Loading } from "./src/components/Loading";
-import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,9 +20,13 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
+import { Alert } from "react-native";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { Realm, useApp } from "@realm/react";
+
 import backgroundImg from "../../assets/background.png";
 import { Button } from "../../components/Button";
 
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from "@env";
+
 import { Container, Slogan, Title } from "./styles";
 
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+});
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const app = useApp();
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true);
+
+      const { idToken } = await GoogleSignin.signIn();
+      if (idToken) {
+        const credentials = Realm.Credentials.jwt(idToken);
+
+        await app.logIn(credentials);
+      } else {
+        Alert.alert("Aviso", "Não foi possível conectar-se à sua conta google.");
+        setIsAuthenticating(false);
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Aviso", "Não foi possível conectar-se à sua conta google.");
+      setIsAuthenticating(false);
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>
@@ -14,7 +50,11 @@ export function SignIn() {
         Gestão de uso de veículos
       </Slogan>
 
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   );
 }
