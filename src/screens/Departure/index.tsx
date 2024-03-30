f801443d6d1b5e7ebaa4eb2cf29d65b710b0b25b
André Kunde <andrelbkunde@gmail.com>
2024-03-30 07:42:11 -0300

    input components

---
+++ src/screens/Departure/index.tsx
@@ -0,0 +1,25 @@
+import { Header } from "../../components/Header";
+import { LicensePlateInput } from "../../components/LicensePlateInput";
+import { TextAreaInput } from "../../components/TextAreaInput";
+
+import { Container, Content } from "./styles";
+
+export function Departure() {
+  return (
+    <Container>
+      <Header title="Saída" />
+
+      <Content>
+        <LicensePlateInput
+          label="Placa do veículo"
+          placeholder="BRA1234"
+        />
+
+        <TextAreaInput
+          label="Finalidade"
+          placeholder="Vou utilizar o veículo para..."
+        />
+      </Content>
+    </Container>
+  );
+}
